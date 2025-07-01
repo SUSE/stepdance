SUSE/stepdance

commit 56feebd2d44af57e48ce3f7a421f8f7ec0c6c538

Prevent token reuse
Only one certificate can be issued per token, avoid request failure
early and instead return a more helpful message.

Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

web/web.go
@@ -196,11 +196,10 @@ func (s *Stepdance) callbackHandler(w http.ResponseWriter, r *http.Request) {
 
 func (s *Stepdance) certReqHandler(w http.ResponseWriter, r *http.Request) {
 	s.sessionManager.Put(r.Context(), "origPath", "/certificate/request")
-
 	// TOOD: validate session?
 	// currently it will just fail if a bogus token is passed, better would be to return early
 
-	accessToken := s.sessionManager.GetString(r.Context(), "token")
+	accessToken := s.sessionManager.PopString(r.Context(), "token")
 	if accessToken == "" {
 		slog.Debug("Certificate request attempted without token")
 		s.errorHandler(w, r, SD_ERR_TOKEN, "")

web/templates/missing_token.html
@@ -1,5 +1,7 @@
 {{define "content"}}
 <div>
   Missing token, need to <a href="/login">Login</a>?
+  <br>
+  Note you can only request one certificate per session.
 </div>
 {{end}}

commit 84b5e37880faf74781a378c753d146f0b9ff26a5

Prevent token reuse further
Only one certificate can be issued per token, the previous patch was not
sufficient. Now redirect the user to a fresh login for every certificate
request with an already used token, regardless of whether the token was
really already used for a certificate request and not just for other
activity. This is more reliable and given certificate requests are a
powerful operation anyways, the additional login flow is deemed
reasonable.

Fixes: 56feebd2d44a ("Prevent token reuse")
Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

web/web.go
@@ -106,6 +106,8 @@ func (s *Stepdance) errorHandler(w http.ResponseWriter, r *http.Request, sdErr i
 func (s *Stepdance) tokenValidator(w http.ResponseWriter, r *http.Request) (*oauth2.Token, bool) {
 	var token *oauth2.Token
 	if s.sessionManager.Exists(r.Context(), "token") {
+		s.sessionManager.Put(r.Context(), "token_used", true)
+
 		token = s.sessionManager.Get(r.Context(), "token").(*oauth2.Token)
 		if !token.Valid() {
 			slog.Debug("Invalid token")
@@ -123,6 +125,7 @@ func (s *Stepdance) tokenValidator(w http.ResponseWriter, r *http.Request) (*oau
 		if newToken.AccessToken != token.AccessToken {
 			slog.Debug("Writing new token")
 			s.sessionManager.Put(r.Context(), "token", newToken)
+			s.sessionManager.Put(r.Context(), "token_used", false)
 			token = newToken
 		}
 
@@ -240,6 +243,7 @@ func (s *Stepdance) callbackHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	s.sessionManager.Put(r.Context(), "token", oauth2Token)
+	s.sessionManager.Put(r.Context(), "token_used", false)
 	ui, err := s.OidcProvider.UserInfo(s.Ctx, s.Oauth2Config.TokenSource(s.Ctx, oauth2Token))
 	if err != nil || ui.Subject == "" {
 		slog.Error("Failed to query userinfo for subject", "error", err)
@@ -260,6 +264,12 @@ func (s *Stepdance) certReqHandler(w http.ResponseWriter, r *http.Request) {
 	// currently it will just fail if a bogus token is passed, better would be to return early
 
 	slog.Debug("req session", "status", s.sessionManager.Status(r.Context()), "token", s.sessionManager.Token(r.Context()))
+
+	if s.sessionManager.GetBool(r.Context(), "token_used") {
+		slog.Debug("token already used")
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
 
 	token, tok := s.tokenValidator(w, r)
 	if !tok {