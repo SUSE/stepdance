SUSE/stepdance

commit f6cc161c157bb7ecff5da2684e78609b10e76747

ca: allow root certificate from file
for situations where the fingerprint based fetching approach does not
work, for example when the HTTPS certificate is issued from a different
CA.

Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

cert/ca.go
@@ -46,12 +46,19 @@ func (s *Step) Token(subject string) string {
 	return token
 }
 
-func NewStep(caurl string, hash string, dburl string, adminprov string, adminpass string) *Step {
+func NewStep(caurl string, cacert string, hash string, dburl string, adminprov string, adminpass string) *Step {
 	s := new(Step)
 
 	slog.Debug("Initializing CA client ...")
 
-	client, err := ca.NewClient(caurl, ca.WithRootSHA256(hash))
+	var client *ca.Client
+	var err error
+
+	if cacert == "" {
+		client, err = ca.NewClient(caurl, ca.WithRootSHA256(hash))
+	} else {
+		client, err = ca.NewClient(caurl, ca.WithRootFile(cacert))
+	}
 	if err != nil {
 		slog.Error("Could not initiate CA client", "error", err)
 		os.Exit(1)

core/config.go
@@ -35,6 +35,7 @@ type Config struct {
 	BindAddress  string
 	BindPort     int
 	CaAdminProv  string
+	CaCert       string
 	CaUrl        string
 	CaDbUrl      string
 	CaDbRefresh  string

web/server.go
@@ -58,7 +58,7 @@ func NewStepdance(c core.Config) (*Stepdance, string) {
 	s := new(Stepdance)
 	s.Ctx = context.Background()
 
-	s.Step = cert.NewStep(c.CaUrl, c.CaHash, c.CaDbUrl, c.CaAdminProv, c.CaPass)
+	s.Step = cert.NewStep(c.CaUrl, c.CaCert, c.CaHash, c.CaDbUrl, c.CaAdminProv, c.CaPass)
 
 	slog.Debug("Initializing OIDC provider ...")
 

web/web_test.go
@@ -225,6 +225,7 @@ func TestMain(m *testing.M) {
 	// connect to StepCA as deployed by test/setup.sh
 	st.s.Step = cert.NewStep(
 		"https://localhost:9000",
+		"",
 		"9da25f5056fdc3901a827b5e2639af48bef834f17e51a2de15e38e2f775c907e",
 		"postgresql://step:step@localhost/step",
 		"",

commit 84f8599281cdd8f83f516e3e1b714adbff5d1935

ca: align admin provisioner with client options
use same root CA logic for both connections.

Fixes: f6cc161c157b ("ca: allow root certificate from file")
Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

cert/ca.go
@@ -51,14 +51,15 @@ func NewStep(caurl string, cacert string, hash string, dburl string, adminprov s
 
 	slog.Debug("Initializing CA client ...")
 
-	var client *ca.Client
-	var err error
+	var copt ca.ClientOption
 
 	if cacert == "" {
-		client, err = ca.NewClient(caurl, ca.WithRootSHA256(hash))
+		copt = ca.WithRootSHA256(hash)
 	} else {
-		client, err = ca.NewClient(caurl, ca.WithRootFile(cacert))
+		copt = ca.WithRootFile(cacert)
 	}
+
+	client, err := ca.NewClient(caurl, copt)
 	if err != nil {
 		slog.Error("Could not initiate CA client", "error", err)
 		os.Exit(1)
@@ -78,7 +79,7 @@ func NewStep(caurl string, cacert string, hash string, dburl string, adminprov s
 		adminprov = "Admin JWK"
 	}
 
-	prov, err := ca.NewProvisioner(adminprov, "", caurl, []byte(adminpass), ca.WithRootSHA256(hash))
+	prov, err := ca.NewProvisioner(adminprov, "", caurl, []byte(adminpass), copt)
 	if err != nil {
 		slog.Error("Could not initiate CA admin provisioner", "error", err)
 	}